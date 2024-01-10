22ff20b2fe5eb7801df44708d2e82d6d1d27ba59
Oluwapelumi Israel <oluwapelumisotoyinbo@gmail.com>
2024-01-10 20:22:04 +0100

    shadcdn and prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Image from 'next/image'
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen">
+    <main>
       evolution
+      <Button>Click me</Button>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+      voluptatibus modi impedit illum recusandae placeat minus nihil saepe
+      adipisci minima distinctio dolorum pariatur amet aliquam, autem sit
+      numquam! Delectus, voluptatem.
     </main>
   )
 }
