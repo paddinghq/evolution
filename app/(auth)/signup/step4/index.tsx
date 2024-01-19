59368bd4f45933dc9f0151c85693fdebb3d24fb7
Akinuliola Akinbobola Oluwafemi <71355010+phemmyblaze@users.noreply.github.com>
2024-01-19 13:16:39 +0100

    sign up screen 4

--- app/(auth)/signup/step4/index.tsx
+++ app/(auth)/signup/step4/index.tsx
@@ -1,5 +1,18 @@
+import { IoChevronBack } from "react-icons/io5";
 const StepFour = () => {
-    return <div>StepFour</div>
+    return <div className="p-24 bg-[#fff] ">
+        <div className="shadow-lg">
+            <div className="shadow-lg">
+                <IoChevronBack />
+            </div>
+
+            <div className="mt-10 px-6">
+                <h5 >Good job! You’re just one step away</h5>
+            </div>
+        
+        </div>
+
+    </div>
 }
 
 export default StepFour
