a71e3e0a671481b420daddf6665594fcb9cfa70c
Akinuliola Akinbobola Oluwafemi <akinuliola_a@yahoo.com>
2024-01-29 17:13:14 +0100

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import LandingPage from './LandingPage'
+// import LandingPage from './HomePage/page'
+import CreateEvent from "../components/CreateEvent"
 
 export default function Home() {
   return (
     <>
       <div className="hidden lg:block">
-        <LandingPage />
-        
+        {/* <LandingPage /> */}
+        <CreateEvent />
       </div>
       <div className="block lg:hidden">
         mobile & tab design is not ready yet
--- components/CreateEvent/tabcomponent/basic.tsx
+++ components/CreateEvent/tabcomponent/basic.tsx
@@ -1,11 +1,51 @@
 import { Input } from '@/components/ui/input';
+import { Calendar } from '@/components/ui/calendar';
+import { IoCalendar } from 'react-icons/io5';
+
+const eventType = [
+    {id:1, title: "Private"},
+    {id:2, title: "Strictly by Invitation"},
+    {id:3, title: "Open"}
+]
+
+
 const Basic = () => {
+
+   
 
     return(
     <div className="flex rounded-lg flex-col gap-8 px-9 py-8 bg-[#217873] opacity-25 text-black">
         <div className="flex gap-3">
             <Input placeholder='Event Name'/>
-            <Input />
+            <Input  placeholder='Event Type'/>
+        </div>
+        <div className="flex gap-3">
+            <Input placeholder='Event Name'/>
+            <Input  placeholder='Event Type'/>
+        </div>
+        <div className="flex gap-3">
+            <div className="relative flex justify-center items-center">
+            
+			    <Input type='search' className=" px-10 rounded-full" placeholder='Event Date'/>
+
+                <IoCalendar
+                size={24}
+                className="absolute  md:left-3 sm:hidden md:block text-[#7a7a7a]"
+                />
+            
+            </div>
+            <Input  placeholder='Event Type'/>
+        </div>
+        <div className="flex gap-3">
+            <Input placeholder='Event Name'/>
+            <Input  placeholder='Event Type'/>
+        </div>
+        <div className="flex gap-3">
+            <Input placeholder='Event Name'/>
+           
+        </div>
+        <div className="flex gap-3">
+            <Input placeholder='Event Name'/>
         </div>
     </div>
     )
