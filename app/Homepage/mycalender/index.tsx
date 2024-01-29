6f7b5e11d8e9202a471a5844f2efb3e7e42bfd5f
Chinwe Chukwuogor <chukwuogorchinwe@gmail.com>
2024-01-29 17:26:39 +0100

    removed calendar files temporarily

--- app/Homepage/mycalender/index.tsx
+++ app/Homepage/mycalender/index.tsx
@@ -1,20 +1,7 @@
 import React from 'react'
-import {
-  Calendar as BigCalendar,
-  CalendarProps,
-  momentLocalizer,
-} from 'react-big-calendar'
-import 'react-big-calendar/lib/css/react-big-calendar.css'
-import moment from 'moment'
-import BasicCalender from './components/BasicCalender'
-import ControlCalender from './components/ControlCalender'
 
 const MyCalender = () => {
-  return (
-    <div className="mt-6">
-      <BasicCalender />
-    </div>
-  )
+  return <div className="mt-6">MyCalender</div>
 }
 
 export default MyCalender
