02413d7d7282e39071e07937235805c202e35836
Mariam Jelili <maryamjelili96@gmail.com>
2024-01-31 16:13:46 +0100

    Added budget and payment pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
-import CreateEvent from '@/components/CreateEvent'
+
 import LandingPage from './LandingPage'
 
 export default function Home() {
   return (
     <>
       <div className="hidden lg:block">
-        {/* <LandingPage /> */}
-        <CreateEvent/>
+        <LandingPage />
+      
         
-        {/* <LandingPage /> */}
-        {/* <CreateEvent /> */}
       </div>
       <div className="block lg:hidden">
         mobile & tab design is not ready yet
