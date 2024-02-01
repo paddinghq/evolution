d50830ad7cbe42668eb7257b913676e0992c7561
Chinwe Chukwuogor <chukwuogorchinwe@gmail.com>
2024-02-01 23:41:36 +0100

    adding

--- app/User/components/Tabs/Calendar.tsx
+++ app/User/components/Tabs/Calendar.tsx
@@ -1,5 +1,49 @@
+'use client'
+import React, { useState, useRef } from 'react'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import CalendarHeader from '../fullcalendar/CalenderHeader'
+
+interface Event {
+  title: string
+  start: Date | string
+  allDay: boolean
+  id: string
+  end: Date | string
+}
+
+const events: Event[] = [
+  {
+    title: 'Seminar Presentation',
+    start: new Date(), // Provide a valid date or string for start
+    allDay: true,
+    id: '',
+    end: new Date(),
+  },
+]
+
 const Calendar = () => {
-  return <div>Calendar</div>
+  const calendarRef = useRef<any>(null)
+  return (
+    <div className="mt-6">
+      <div className="bg-[#21787333] pt-16 px-16 rounded-2xl flex flex-col gap-10">
+        <CalendarHeader calendarRef={calendarRef} />
+        <FullCalendar
+          ref={calendarRef}
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          initialView={'dayGridMonth'}
+          headerToolbar={false}
+          height={'100vh'}
+          themeSystem={'Sandstone'}
+          editable={true}
+          selectable={true}
+          events={events}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default Calendar
---
+++ app/User/components/fullcalendar/CalenderHeader.tsx
@@ -0,0 +1,121 @@
+import React, { useState, RefObject, useRef, useEffect } from 'react'
+import moment from 'moment'
+import { Button } from '@/components/ui/button'
+import { IoSearchSharp } from 'react-icons/io5'
+import { Input } from '@/components/ui/input'
+import { FaArrowLeftLong } from 'react-icons/fa6'
+import { FaArrowRightLong } from 'react-icons/fa6'
+
+interface HeaderProps {
+  calendarRef: RefObject<any> // Replace 'any' with the appropriate type of your calendarRef
+}
+
+const CalendarHeader: React.FC<HeaderProps> = ({ calendarRef }) => {
+  const [title, setTitle] = useState<string>(moment().format('MMMM DD, YYYY'))
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    const calendarApi = calendarRef.current.getApi()
+
+    // Update the input value when the calendar view changes
+    const updateTitle = () => {
+      setTitle(calendarApi.view.title)
+    }
+
+    calendarApi.on('viewTitleChange', updateTitle)
+
+    return () => {
+      // Cleanup event listener on unmount
+      calendarApi.off('viewTitleChange', updateTitle)
+    }
+  }, [calendarRef])
+
+  const handleInputChange = () => {
+    // Do something with the input value, if needed
+    console.log('Input value:', inputRef.current?.value)
+  }
+
+  const handleDay = () => {
+    const calendarApi = calendarRef.current.getApi()
+    calendarApi.changeView('timeGridDay')
+  }
+
+  const handleWeek = () => {
+    const calendarApi = calendarRef.current.getApi()
+    calendarApi.changeView('timeGridWeek')
+  }
+
+  const handleMonth = () => {
+    const calendarApi = calendarRef.current.getApi()
+    calendarApi.changeView('dayGridMonth')
+  }
+
+  const handleYear = () => {
+    const calendarApi = calendarRef.current.getApi()
+    calendarApi.changeView('dayGridYear')
+  }
+
+  return (
+    <div className="flex justify-between gap-3">
+      <div className="flex gap-4 ">
+        <Button
+          className="text-[#252C2B] hover:bg-[#2A6562] hover:text-white mt-[0.3rem] w-20 h-10 bg-transparent  border-solid border-2 border-slate-600"
+          onClick={() => calendarRef.current.getApi().prev()}
+        >
+          <FaArrowLeftLong size={22} />
+        </Button>
+        <Button
+          className="text-[#252C2B] hover:bg-[#2A6562] hover:text-white w-20 h-12  bg-transparent  border-solid border-2 border-slate-600"
+          onClick={() => calendarRef.current.getApi().today()}
+        >
+          Today
+        </Button>
+        <Button
+          className="text-[#252C2B] hover:bg-[#2A6562] hover:text-white mt-[0.3rem] w-20 h-10 bg-transparent  border-solid border-2 border-slate-600"
+          onClick={() => calendarRef.current.getApi().next()}
+        >
+          <FaArrowRightLong size={22} />
+        </Button>
+      </div>
+      <div className="flex gap-4">
+        <Button
+          className="bg-transparent text-[#7F7F7F] hover:bg-[#2A6562] hover:text-white"
+          onClick={handleDay}
+        >
+          Day
+        </Button>
+        <Button
+          className="bg-transparent text-[#7F7F7F] hover:bg-[#2A6562] hover:text-white"
+          onClick={handleWeek}
+        >
+          Week
+        </Button>
+        <Button
+          className="bg-transparent text-[#7F7F7F] hover:bg-[#2A6562] hover:text-white"
+          onClick={handleMonth}
+        >
+          Month
+        </Button>
+        <Button
+          className="bg-transparent text-[#7F7F7F] hover:bg-[#2A6562] hover:text-white"
+          onClick={handleYear}
+        >
+          Year
+        </Button>
+      </div>
+      <div className="relative flex justify-center items-center">
+        <IoSearchSharp
+          size={24}
+          className="absolute  md:left-3 sm:hidden md:block text-[#7a7a7a]"
+        />
+        <Input
+          type="search"
+          className="w-50 px-10 rounded-full focus-visible:ring-0 focus-visible:ring-offset-0"
+          placeholder="Search"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default CalendarHeader
