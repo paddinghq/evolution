c77dae94647c87705a4126999634f3547abcc8f0
Akinuliola Akinbobola Oluwafemi <akinuliola_a@yahoo.com>
2024-02-01 15:51:52 +0100

    correction

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
-
 import LandingPage from './LandingPage'
-
 
 export default function Home() {
   return (
     <>
       <div className="hidden lg:block">
         <LandingPage />
-        
       </div>
       <div className="block lg:hidden">
         mobile & tab design is not ready yet
