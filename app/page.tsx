c61dcd4ce9c429f885f2f24cbc162df47b310417
Akinuliola Akinbobola Oluwafemi <akinuliola_a@yahoo.com>
2024-02-05 01:12:34 +0100

    Redux setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import LandingPage from './LandingPage'
+import { Provider } from 'react-redux';
+import {store }from './../lib/Store';
 
 export default function Home() {
   return (
-    <>
+    <Provider store={store}>
       <div className="hidden lg:block">
         <LandingPage />
       </div>
       <div className="block lg:hidden">
         mobile & tab design is not ready yet
       </div>
-    </>
+    </Provider>
   )
 }
