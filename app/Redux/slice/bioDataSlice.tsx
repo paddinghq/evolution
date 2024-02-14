20198b5452d88aeebb6b2a1791087b900fd3ed07
Akinuliola Akinbobola Oluwafemi <akinuliola_a@yahoo.com>
2024-02-14 17:08:33 +0100

    update

--- app/Redux/slice/bioDataSlice.tsx
+++ app/Redux/slice/bioDataSlice.tsx
@@ -19,6 +19,26 @@ export const BioSlice = createSlice({
     name: "bioData",
     initialState, 
     reducers:{
-        
+        setBioData: (state, action) => {
+            state.bioData = {...state.bioData, ...action.payload}
+        },
+        setSteps: (state, action) => {
+            state.steps = state.steps +1
+        },
+        decrementSteps: (state) => {
+			if (state.steps > 0) {
+				state.steps = state.steps - 1;
+			}
+		},
     }
 })
+
+
+export const {
+    setBioData,
+    setSteps,
+	decrementSteps,
+} = BioSlice.actions;
+
+
+export default BioSlice.reducer
