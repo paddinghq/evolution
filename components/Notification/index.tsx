b27dbf8af695c0c4f2705a3b8ddf2a9e05f603b1
Oluwapelumi Israel <oluwapelumisotoyinbo@gmail.com>
2024-02-15 13:02:38 +0100

    build fails

--- components/Notification/index.tsx
+++ components/Notification/index.tsx
@@ -1,11 +1,13 @@
+'use client'
 import { IoIosNotificationsOutline } from 'react-icons/io'
-import { FaCircle } from "react-icons/fa";
+import { FaCircle } from 'react-icons/fa'
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
 import * as React from 'react'
+import { useState } from 'react'
 import { ScrollArea } from '@/components/ui/scroll-area'
 import { Separator } from '@/components/ui/separator'
 
@@ -15,17 +17,29 @@ const notifications: string[] = [
   'notification3',
   'notification4',
   'notification5',
-  'notification16',
+  'notification6',
 ]
 
 const Notification = () => {
+  const [notification, setNotification] = useState<boolean>(true)
+
+  const closeNotification = () => {
+    setNotification(false)
+    console.log('closed')
+  }
+
   return (
     <div>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <div className="cursor-pointer relative">
+          <div className=" relative bottom-5" onClick={closeNotification}>
             <IoIosNotificationsOutline className="text-white text-3xl" />
-            <FaCircle className='text-orange-500 w-3 absolute bottom-[1.1rem] right-1' />
+            <FaCircle
+              data-state="close"
+              className={`text-orange-500 w-3 absolute bottom-[1.1rem] right-1 ${
+                notification ? '' : 'hidden'
+              }`}
+            />
           </div>
         </DropdownMenuTrigger>
         <DropdownMenuContent className="w-72">
